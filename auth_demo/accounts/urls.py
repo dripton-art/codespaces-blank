deadd2723a690eed595689c8c2fc11eb8e9b23f4
ivan <ivankaweesa.biz@gmail.com>
2025-07-23 13:08:18 +0000

    updated

--- auth_demo/accounts/urls.py
+++ auth_demo/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('secret/', views.secret_page, name='secret'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
