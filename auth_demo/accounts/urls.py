65528b665763a0872254666865d7e2833af8e764
ivan <ivankaweesa.biz@gmail.com>
2025-07-23 13:10:32 +0000

    updated

--- auth_demo/accounts/urls.py
+++ auth_demo/accounts/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('secret/', views.secret_page, name='secret'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
